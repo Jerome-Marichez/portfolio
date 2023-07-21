164e770ed21d362878fa5193014b510001e839c8
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-21 18:37:39 +0200

    CodeBlock Component #2
    An issue occurred in production mode with the react-highlight library, specifically the <Highlight/> component, where the syntax highlighting wasn't functioning correctly.
    
    The text remained in the default color, indicating that the expected highlighting wasn't applied.
    
    To resolve this issue, we implemented the following changes:
    
    - Instead of relying on the react-highlight library, we directly imported the original highlight.js library (written in JavaScript) into our custom <CodeBlock/> component.
    
    - We took control of loading the highlight.js library ourselves and ensured that it was fully loaded and executed before displaying the code block.
    
    - Display the codeblock
    
    -Additionally, we improved the flexibility of the <CodeBlock/> component by allowing to choose the font size using a class-based approach.
    We can now select from "small", "medium", or "big" font sizes for the code block.
    This also sets the stage for future responsiveness by utilizing media queries.
    
    With these adjustments, the <Highlight/> component now functions as intended in both development and production environments, and the syntax highlighting is properly applied to the code blocks.

--- src/app/components/CodeBlock/CodeBlock.tsx
+++ src/app/components/CodeBlock/CodeBlock.tsx
@@ -1,21 +1,20 @@
-
 'use client'
-import { CSSProperties } from "react";
-import Highlight from 'react-highlight'
+import { CSSProperties, useEffect } from "react";
 import { motion } from 'framer-motion';
 import './CodeBlock.scss';
+import hljs from './highlight.js_11.7.0.min.js';
 
 interface CodeBlockProps {
 	text: string;
 	margin?: string;
-	fontSize: number;
+	fontSize: "small" | "medium" | "big";
 	language: "typescript" | "javascript" | "java" | "php";
 }
 
 /**
 * @param text The text to be displayed as a code block
 * @param margin Margin as top | right | bottom | left (in pixels)
-* @param fontSize Display the text with the requested font size (in pixels)
+* @param fontSize Display the text with the requested style
 * @param language The format (language to be used) for formatting
 * @returns A code block that displays lines of code with color and formatting
 **/
@@ -23,14 +22,20 @@ interface CodeBlockProps {
 export function CodeBlock(props: CodeBlockProps): JSX.Element {
 
 	const marginStyle: CSSProperties = props.margin ? { margin: props.margin } : {};
-	const finalStyle: CSSProperties = { ...marginStyle, fontSize: props.fontSize };
+
+	useEffect(() => {
+		(async () => {
+			// @ts-ignore
+			await hljs.highlightAll();
+		})();
+	}, []);
 
 	return (
-
-		<motion.div className={'codeblock'} style={finalStyle}>
-			<Highlight language="typescript">
-				{props.text}
-			</Highlight>
-		</motion.div>
+		<motion.div className={`codeblock--${props.fontSize}`} style={marginStyle}>
+			<pre>
+				<code className={`language-${props.language}`}>{props.text}</code>
+			</pre>
+		</motion.div >
 	)
+
 }
