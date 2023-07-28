789524246caef61a3e1a9e2e7286d7820e6db1d8
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-28 10:20:51 +0200

    About Page
    About Page created with their styles and their content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <main className={styles.home}>
       <h1 style={{ display: "none" }}>{title}</h1>
       <TypingTitle title={title} speed={3.5} />
-      <div className="home-flex"><Link href={linksData[1].href}><Button height={52} width={220} text={"portfolio"} /></Link> </div>
+      <div className={styles.home}><Link href={linksData[1].href}><Button height={52} width={220} text={"portfolio"} /></Link> </div>
     </main>
   
   )
