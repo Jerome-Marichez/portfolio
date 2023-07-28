e903d006b4338224eaec29853a3b03a954b464b4
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-28 08:19:19 +0200

    Page SCSS Home outside global
    Page SCSS Home outside global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@ import { Button } from "./components";
 import Link from "next/link";
 import { linksData } from "./routes";
 import { TypingTitle } from "./components";
-
+import styles from "./page.module.scss"; 
 /**
  * @returns The home page 
  */
 export default function Home() {
 
-  const title: string = `< i'm marichez jérôme, web developper >`;
+  const title: string = `< i'm marichez jérôme, web developer >`;
 
   return (
 
-    <main className="home-flex">
+    <main className={styles.home}>
       <h1 style={{ display: "none" }}>{title}</h1>
       <TypingTitle title={title} speed={3.5} />
       <div className="home-flex"><Link href={linksData[1].href}><Button height={52} width={220} text={"portfolio"} /></Link> </div>
