53d991c11031a65bc16d2b8a2ac1675c92cdb111
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-28 07:46:05 +0200

    ProfilPic Component
    Created a profile pic component which display our profile pic

---
+++ src/app/components/ProfilPic/ProfilPic.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import profilPic from "./profilPic.jpg";
+import styles from "./ProfilPic.module.scss";
+import { motion } from "framer-motion";
+
+
+/**
+ * @returns A component profile pic with a shape circle
+ */
+export default function ProfilPic(): JSX.Element {
+
+	return (
+		<motion.div
+			initial={{ opacity: 0.5, scale: 0.5 }}
+			animate={{ opacity: 1.0, scale: 1.0, filter: 'grayscale(80%)' }}
+			whileHover={{ filter: 'grayscale(0%)' }}
+			transition={{ ease: "easeOut", duration: 0.5 }}
+		>
+			<Image
+				className={styles.profil}
+				src={profilPic}
+				alt={"Picture of me"}
+			/>
+		</motion.div>
+	)
+
+}
