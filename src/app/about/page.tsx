016a26b723fdd97f8189c435a3e552f95abfae85
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-28 13:26:32 +0200

    MetaData doesn't support 'use client'
    Moved content inside page.tsx for About Page into a new component AboutClient() so we can now still support metadata.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
-'use client'
+import { AboutClient } from './pageclient'
 import type { Metadata } from 'next'
-import styles from "./page.module.scss";
-import { motion } from 'framer-motion';
 
 export const metadata: Metadata = {
   title: 'Jérôme Marichez - About',
@@ -10,16 +8,6 @@ export const metadata: Metadata = {
 
 export default function About(): JSX.Element {
   return (
-    <motion.div className={styles.about} initial={{ translateX: 1000 }} animate={{ translateX: 0 }} transition={{ duration: 0.5 }}>
-      <h2>Developer</h2>
-      <h1>Web Developer</h1>
-      <div className={styles.text}>
-        <p>Web developer specialized in React, RNCP certified graduate with a Bachelor degree (equivalent to bac+4). </p>
-        <p>Proficient in various aspects of web development, including mastery of components, virtual rendering, routing, and state management.</p>
-        <p>Proficient in creating dynamic and high-performance interfaces, as well as handling their back-end functionalities.</p>
-        <p>Passionate about innovation and enhancing user experience.
-        </p>
-      </div>
-    </motion.div>
+    <AboutClient />
   )
 }
---
+++ src/app/about/pageclient.tsx
@@ -0,0 +1,20 @@
+'use client'
+import styles from "./page.module.scss";
+import { motion } from 'framer-motion';
+
+
+export function AboutClient() {
+	return (
+		<motion.div className={styles.about} initial={{ translateX: 1000 }} animate={{ translateX: 0 }} transition={{ duration: 0.5 }}>
+			<h2>Developer</h2>
+			<h1>Web Developer</h1>
+			<div className={styles.text}>
+				<p>Web developer specialized in React, RNCP certified graduate with a Bachelor degree (equivalent to bac+4). </p>
+				<p>Proficient in various aspects of web development, including mastery of components, virtual rendering, routing, and state management.</p>
+				<p>Proficient in creating dynamic and high-performance interfaces, as well as handling their back-end functionalities.</p>
+				<p>Passionate about innovation and enhancing user experience.
+				</p>
+			</div>
+		</motion.div>
+	)
+}
