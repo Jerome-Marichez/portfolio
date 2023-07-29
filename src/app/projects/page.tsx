970785906335872647bc5b2542dd7049692063ca
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-29 07:55:50 +0200

    Projects Page
    Created our projects page and modified the project so it loaded it correctly if we are at projects.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,9 @@ export const metadata: Metadata = {
 }
 
 export default function Projects(): JSX.Element {
+
 	return (
 		<ProjectsClient />
 	)
+	
 }
--- src/app/projects/pageclient.tsx
+++ src/app/projects/pageclient.tsx
@@ -1,12 +1,62 @@
 'use client'
 import styles from "./page.module.scss";
 import { motion } from 'framer-motion';
+import { Card } from "../components";
 
+export function ProjectsClient(): JSX.Element {
 
-export function ProjectsClient() {
 	return (
 		<motion.div className={styles.projects} initial={{ translateX: 1000 }} animate={{ translateX: 0 }} transition={{ duration: 0.5 }}>
-			
+			<div className={styles.grid}>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+				<Card
+					image="https://user-images.githubusercontent.com/62617561/253905231-651cb6b7-f5fd-4d03-a171-5a0182463962.jpg"
+					title="Boule Infernal"
+					tag={['React', 'Typescript', 'Html', 'Scss']}
+				/>
+			</div>
+
 		</motion.div>
 	)
+
 }
