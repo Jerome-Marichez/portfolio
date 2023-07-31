d16377a2164cf9b0ebe2e097e94cea810c44bcb9
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-31 13:49:08 +0200

    Button Component #2
    - Add some color change to the button.
    - Added some documentation
    - Height & Width props are now optional.

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,24 +1,28 @@
+
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
-	height: number;
-	width: number;
+	height?: number;
+	width?: number;
 	text: string;
 }
 
 /**
- * 
- * @param props 
- * @returns A button component 
+ * @param height A optional height for the button 
+ * @param width A optional width for the button 
+ * @param text The text to display on the button 
+ * @returns A styled button component which display a text
  */
 export function Button(props: ButtonProps): JSX.Element {
+	
+	const { height, width, text } = props;
 
 	return (
 		<button
 			className={styles.button}
-			style={{ height: props.height, width: props.width }}
+			style={height && width ? { height: height, width: width } : {}}
 		>
-			{props.text}
+			{text}
 		</button>
 	)
 
