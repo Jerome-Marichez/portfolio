f114349bc3063718a8f80abdab75307d1f19890e
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-31 18:06:04 +0200

    Update pageclient.tsx
    Mistake about two time CSS in pageclient.tsx

--- src/app/skills/pageclient.tsx
+++ src/app/skills/pageclient.tsx
@@ -14,17 +14,17 @@ export function SkillsClient(): JSX.Element {
 			<SkillItem height={80} width={80} type="react" />
 			<SkillItem height={80} width={80} type="typescript" />
 			<SkillItem height={80} width={80} type="nextjs" />
-			<SkillItem height={80} width={80} type="css" />
 			<SkillItem height={80} width={80} type="javascript" />
+			<SkillItem height={80} width={80} type="jest" />
 			<SkillItem height={80} width={80} type="sass" />
 			<SkillItem height={80} width={80} type="css" />
 			<SkillItem height={80} width={80} type="html5" />
-			<SkillItem height={80} width={80} type="jest" />
 			<SkillItem height={80} width={80} type="mongodb" />
 			<SkillItem height={80} width={80} type="mysql" />
 			<SkillItem height={80} width={80} type="postgresql" />
 			<SkillItem height={80} width={80} type="github" />
 			<SkillItem height={80} width={80} type="php" />
+			<SkillItem height={80} width={80} type="wordpress" />
 			<SkillItem height={80} width={80} type="codeigniter" />
 		</motion.div>
 	)
