db551d28f3e7cba23eb1f8c5cbd9e1069927363d
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-08-03 19:13:32 +0200

    A cup of coffee #7
    - I have created a subcomponent folder within the MugCoffee component, where two subcomponents, namely <Title/> and <Coffee/>, have been added. This improvement makes the MugCoffee component clearer and simpler.
    
    This makes the MugCoffee component clearer and simpler.
    
    - Additionally, I have implemented a fallback feature for mobile and tablet devices, using pictures instead of videos in cases where video loops are not supported without user interaction for the coffee subcomponent

---
+++ src/app/components/MugCoffee/subcomponent/Coffee/Coffee.tsx
@@ -0,0 +1,71 @@
+
+import { motion } from "framer-motion"
+import styles from "./Coffee.module.scss";
+import coffeePic from "./Coffee.jpg";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+interface CoffeeProps {
+	durationAnimation: number;
+	height: number;
+	width: number;
+}
+
+/**
+ * @param durationAnimation duration of animation in seconds.
+ * @param width Width of the coffee.
+ * @param height Height of the coffee.
+ * @returns A coffee video or picture whith a rotation animation.
+ */
+export function Coffee(props: CoffeeProps): JSX.Element {
+
+	const [isSmallScreen, setSmallScreen] = useState<boolean>(false);
+
+	const resize = () => {
+		if (window.innerWidth < 1200) {
+			setSmallScreen(true);
+		} else {
+			setSmallScreen(false);
+		}
+	}
+	useEffect(() => {
+		resize();
+		window.addEventListener("resize", resize, false);
+
+		return () => { window.removeEventListener("resize", resize, false); };
+	}, [])
+
+
+
+	return (
+		<motion.div animate={{ rotate: ['0deg', '360deg'] }}
+			className={styles.coffee}
+			transition={{
+				type: "keyframes",
+				duration: props.durationAnimation,
+				repeat: Infinity,
+			}}
+		>
+			{isSmallScreen ?
+				<Image
+					src={coffeePic}
+					width={props.width - 25}
+					height={props.height - 18}
+					alt="Picture of the coffee"
+					priority={true}
+				/>
+
+				:
+				<video autoPlay muted loop
+					width={props.width}
+					height={props.height}
+					aria-label="Video of coffee"
+				>
+					<source src={require('./coffee.mp4')} type="video/mp4" />
+				</video>
+			}
+		</motion.div>
+
+	)
+}
+
